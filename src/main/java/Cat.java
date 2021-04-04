8d6c94be34d78e07bb04feff73e05d2e3e5a9cb4
aleksey <alesej88@ya.ru>
2021-04-04 11:50:39 +0600

    Home work 7

---
+++ src/main/java/Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+    private String name;
+    private int appetite;
+
+    public Cat(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+    }
+    
+}
+}
